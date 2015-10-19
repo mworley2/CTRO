78539f6854fd88c7d0442d1f43f07ad23b4d3326
kleinhansjy <kleinhansjy@gmail.com>
2015-10-19 14:31:10 -0500

    push

---
+++ upload_case.php
@@ -0,0 +1,15 @@
+<?php
+
+// include the configs / constants for the database connection
+require_once("config/db.php");
+
+// load the registration class
+require_once("classes/Upload_Case.php");
+
+// create the upload object. when this object is created, it will do all upload stuff automatically
+// so this single line handles the entire upload process.
+$upload = new Upload_Case();
+
+// show the register view (with the registration form, and messages/errors)
+include("views/upload_case.php");
+
