1351cbdf96bc14e1f51f25f18b17ad7c3428a4e4
kleinhansjy <kleinhansjy@gmail.com>
2015-10-19 13:52:16 -0500

    Working on splitting and storing PDFs.
    DECISION: Blob storage vs path storage

---
+++ classes/Upload_Case.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * @var object $db_connection The database connection
+ */
+class Upload_Case
+{
+    private $db_connection = null;
+    /**
+     * @var array $errors Collection of error messages
+     */
+    public $errors = array();
+    /**
+     * @var array $messages Collection of success / neutral messages
+     */
+    public $messages = array();
+
+
+    public function __construct()
+    {
+        if (isset($_POST["register"])) {
+            $this->uploadNewCase();
+        }
+    }
+    private function uploadNewCase()
+    {
+        require_once("config/db.php");
+        $this->db_connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+        $myPDF = 'imaginary string';
+        $nextCaseID = 9;
+
+       // $this->split_pdf($myPDF, $nextCaseID, $this->db_connection);
+        /**
+         * Split PDF file and upload pieces
+         * partial: https://gist.github.com/maccath/3981205
+         */
+
+        require_once('../libraries/PDFTools/fpdf17/fpdf.php');
+        require_once('../libraries/PDFTools/fpdi/fpdi.php');
+        if (!$this->db_connection)
+            die("Can't connect to MySQL: " . mysqli_connect_error());
+
+        $pdf = new FPDI();
+        $pagecount = $pdf->setSourceFile($myPDF); // How many pages?
+
+        if ($pagecount > 20)
+            die("That PDF is too many pages and will clog our database");
+
+        /**
+         * https://blogs.oracle.com/oswald/entry/php_s_mysqli_extension_storing For perpared statements
+         */
+
+        // Split each page into a new PDF
+        for ($i = 1; $i <= $pagecount; $i++) {
+            $new_pdf = new FPDI();
+            $new_pdf->AddPage();
+            $new_pdf->setSourceFile($myPDF);
+            $new_pdf->useTemplate($new_pdf->importPage($i));
+
+            $new_pdf->Output('unused', "R");
+
+            $stmt = $this->db_connection->prepare("INSERT INTO slides (image) VALUES(?)");
+            $null = NULL;
+            $stmt->bind_param("b", $null);
+            $stmt->send_long_data(0, $new_pdf);
+            $stmt->execute();
+
+        }
+    }
+
+/** refactor this out later
+    private function split_pdf($filename, $caseUploadID, $mysqli)
+    {
+
+    }
+
+ */
+ }
--- views/logged_in.php
+++ views/logged_in.php
@@ -2,7 +2,7 @@
 Hey, <?php echo $_SESSION['user_name']; ?>. You are logged in.
 Try to close this browser tab and open it again. Still logged in! ;)
 
-
+<a href="upload_case.php"> Upload New Case</a>
 
 <!-- because people were asking: "index.php?logout" is just my simplified form of "index.php?logout=true" -->
 <a href="index.php?logout">Logout</a>
