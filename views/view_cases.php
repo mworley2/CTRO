83f68508f9194b71cb363f5ca3d100389479d935
kleinhansjy <kleinhansjy@gmail.com>
2015-10-19 21:05:40 -0500

    Need to get views working with sessions

--- views/view_cases.php
+++ views/view_cases.php
@@ -12,25 +12,16 @@ if (isset($view)) {
         }
     }
 }
+//TODO figure out how sessions work and get these working so we can pull this users cases and stuff or however that works?
+$myID = $_SESSION['user_id'];
+
+if($_SESSION['user_id'] < 10)
+    echo ('<a href="index.php">Special Back ' . $myID . ' to Home Page</a>'); // does this work?
+
 ?>
 
-<!-- register form -->
-<form method="post" action="upload_case.php" name="Case Upload Form">
-
-    <!-- the user name input field uses a HTML5 pattern check -->
-    <label for="upload_input_name">Case Name</label>
-    <input id="upload_input_name" class="upload_input" type="text" name="case_name" required />
-
-    <!-- the email input field uses a HTML5 email type check -->
-    <label for="upload_input_style">Case Style</label>
-    <input id="upload_input_style" class="upload_input" type="text" name="case_style" required />
-
-    <!-- TODO Server side verification on allowable inputs (pdf) -->
-
-    <label for="upload_input_slides"> Slides Upload</label>
-    <input type="file" placeholder="Upload file"  name="slides_pdf" id="upload_input_slides" accept="application/pdf" class="required" />
-    <input type="submit"  name="Case Upload" value="Upload_Case" />
-</form>
 
 <!-- backlink -->
 <a href="index.php">Back to Home Page</a>
+
+<?php echo $_SESSION['user_name']; ?>
