71d4f8844ad2dc2788505625e756871af8078950
kleinhansjy <kleinhansjy@gmail.com>
2015-10-20 23:44:10 -0500

    Deletion MVP

--- delete.php
+++ delete.php
@@ -1,8 +1,21 @@
 <?php
 // include the configs / constants for the database connection
 require_once("config/db.php");
+
 // load the registration class
-require_once("classes/Upload_Case.php");
+require_once("classes/Delete.php");
+
+$delete = new Delete();
+
 // show the register view (with the registration form, and messages/errors)
-include("views/view_cases.php");
-//TODO get rid of the require once wtf is that doing here
+?>
+<!--
+<html>
+<head>
+    <title>Success</title>
+</head>
+
+<body onload="window.back()">
+<h1>Success</h1>
+</body>
+</html>
