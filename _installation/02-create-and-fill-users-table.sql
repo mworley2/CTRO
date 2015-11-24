e389af1da8f48ffd9bb8a90a43582f7616af7434
kleinhansjy <kleinhansjy@gmail.com>
2015-11-24 14:40:38 -0600

    Splitting slides fixed, no images

--- _installation/02-create-and-fill-users-table.sql
+++ _installation/02-create-and-fill-users-table.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS login;
   PRIMARY KEY (`user_id`, `user_name`, `user_email`) would mean no two tuples can have same values for all 3 of these attributes
   but we want to make sure no two users have same username
 */
-CREATE TABLE IF NOT EXISTS `login`.`users` (
+CREATE TABLE IF NOT EXISTS  `users` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing user_id of each user, unique index',
   `user_name` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s name, unique',
   `user_password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s password in salted and hashed format',
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS `login`.`users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='user data';
 
 /* Case(caseID, name, style, numSlides, timesTaken, averageTime) */
-CREATE TABLE IF NOT EXISTS `login`.`cases` (
+CREATE TABLE IF NOT EXISTS  `cases` (
   `case_id` int(11) AUTO_INCREMENT COMMENT 'auto incrementing case_id of each case, unique index',
   `case_name` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'case''s name',
   `style` varchar(255) COLLATE utf8_unicode_ci  COMMENT 'style of case interview',
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS `login`.`cases` (
 /* Interviews(interviewID, permissions, giverUsername, takerUsername, notes, timeTaken) 
 same giver and taker pair can do multiple interviews(interviewIDs)
 */
-CREATE TABLE IF NOT EXISTS `login`.`interviews`  (
+CREATE TABLE IF NOT EXISTS  `interviews`  (
   `interview_id` int(11)  AUTO_INCREMENT COMMENT 'auto incrementing interview_id of each interview, unique index',
   `permissions` int(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'permissions of the case',
   `giver_username` varchar(64) COLLATE utf8_unicode_ci  COMMENT 'giver of the case interview, unique',
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `login`.`interviews`  (
 /* EngagesIn(username, interviewID) 
 user may be in table >1 time and interviewID in table 2 times (giver and taker) <- way to implement this constraint?
 */ 
-CREATE TABLE IF NOT EXISTS `login`.`engagesin` (
+CREATE TABLE IF NOT EXISTS  `engagesin` (
   `user_name` varchar(64)  COMMENT 'user_name of the user engaging in the case, unique',
   `interview_id` int(11) COLLATE utf8_unicode_ci COMMENT 'id of interview, unique',
   PRIMARY KEY (`user_name`, `interview_id`)
@@ -50,7 +50,7 @@ CREATE TABLE IF NOT EXISTS `login`.`engagesin` (
 /* Owns(username, caseID) 
 a user can own multiple cases, but each case can only have one owner <- how to enforce??
 */
-CREATE TABLE IF NOT EXISTS `login`.`owns` (
+CREATE TABLE IF NOT EXISTS  `owns` (
   `user_name` varchar(64)  COMMENT 'user_name of the user engaging in the case, unique',
   `case_id` int(11) COLLATE utf8_unicode_ci  COMMENT 'id of the case, unique',
   PRIMARY KEY (`case_id`, `user_name`)
@@ -59,7 +59,7 @@ CREATE TABLE IF NOT EXISTS `login`.`owns` (
 /* Slides(caseID, slideNum, slidePDF) 
   blob = varbinary(MAX) ??
 */
-CREATE TABLE IF NOT EXISTS `login`.`slides` (
+CREATE TABLE IF NOT EXISTS  `slides` (
   `case_id` int(11) COLLATE utf8_unicode_ci  COMMENT 'id of the case, unique',
   `slide_num` int(64) COLLATE utf8_unicode_ci COMMENT 'slide number in the case (order in case)',
   `slide_pdf` blob NOT NULL COMMENT 'actual slide pdf contents ',
@@ -67,7 +67,7 @@ CREATE TABLE IF NOT EXISTS `login`.`slides` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='individual slides for all cases';
 
 /* Uses(interviewID, caseID) */
-CREATE TABLE IF NOT EXISTS `login`.`uses` (
+CREATE TABLE IF NOT EXISTS  `uses` (
   `interview_id` int(11) COLLATE utf8_unicode_ci COMMENT 'id of the interview, unique',
   `case_id` int(11) COLLATE utf8_unicode_ci  COMMENT 'id of the case, unique',
   PRIMARY KEY (`interview_id`, `case_id`)
--- classes/Upload_Case.php
+++ classes/Upload_Case.php
@@ -1,5 +1,5 @@
 <?php
-
+ini_set('display_errors', 1);
 class Upload_Case
 {
     /**
@@ -31,13 +31,19 @@ class Upload_Case
 
         $case_name = $this->db_connection->real_escape_string(strip_tags($_POST['case_name'], ENT_QUOTES));
         $case_style = $this->db_connection->real_escape_string(strip_tags($_POST['case_style'], ENT_QUOTES));
+        $case_slides = $this->db_connection->real_escape_string(strip_tags($_POST['slides_pdf'], ENT_QUOTES));
 
-        $num_slides = 10; //TODO once PDF splitting works
+        require_once('fpdf17/fpdf.php');
+        require_once('fpdi/fpdi.php');
+        $pdf = new FPDI();
+        print_r($case_slides);
+        $num_slides = $pdf->setSourceFile($case_slides); //TODO once PDF splitting works
 
         $sql = "INSERT INTO cases (case_id, case_name, style, num_slides, times_taken, avg_time)
                             VALUES(NULL, '" . $case_name . "', '" . $case_style . "', '" . $num_slides . "', '0','0.0');";
 
         $query_new_owns_insert = $this->db_connection->query($sql);
+        //$case_id = 0;
 
         if ($query_new_owns_insert) {
 
@@ -48,13 +54,54 @@ class Upload_Case
 
             $query_new_owns_insert = $this->db_connection->query($sql);
 
-            if($query_new_owns_insert)
-                echo "SUCCESS IN OWNS";
+            if(!$query_new_owns_insert)
+                $this->errors[] = 'Sorry, your case upload failed. Please go back and try again. -> Failure to insert into owns table';
             else
-                echo "FAIL IN OWNS";
+            {
+                //TODO Make all of the sql entries contingent on eachother
+                $this->split_pdf($case_slides, $case_id);
+            }
 
         } else {
             $this->errors[] = "Sorry, your case upload failed. Please go back and try again.";
         }
+
+
     }
+    // https://gist.github.com/maccath/3981205
+    private function split_pdf($filename, $case_id)
+    {
+        require_once('fpdf17/fpdf.php');
+        require_once('fpdi/fpdi.php');
+
+        $base_directory = 'resources/slide_storage/';
+
+
+        $pdf = new FPDI();
+        $pagecount = $pdf->setSourceFile($filename); // How many pages?
+
+        // Split each page into a new PDF
+        for ($i = 1; $i <= $pagecount; $i++)
+        {
+            echo $i;
+            $new_pdf = new FPDI();
+            $new_pdf->AddPage('L');
+            $new_pdf->setSourceFile($filename);
+            $new_pdf->useTemplate($new_pdf->importPage($i)); // Is this actually a PDF yet?
+
+            $new_path = $base_directory . "c" . $case_id . "p" . $i . '.pdf';
+            $new_pdf->Output($new_path, "F");
+
+           /* $im = new Imagick();
+            $im->setResolution(300,300); //TODO wtf res do we want?
+            $im->readimage($new_pdf);
+            $im->setImageFormat('jpeg');
+            $im->writeImage($new_path);
+            $im->clear();
+            $im->destroy();
+           */
+        }
+
+    }
+
 }
