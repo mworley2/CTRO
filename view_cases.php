e2be600746808b318b4eea3d3ea2cf92de6fdb1e
mworley2 <mworley2@illinois.edu>
2015-12-03 06:18:54 -0600

    view cases

--- view_cases.php
+++ view_cases.php
@@ -2,25 +2,36 @@
 // include the configs / constants for the database connection
 ini_set('display_errors', 1);
 require_once("config/db.php");
-
-include("views/view_cases.php");
-
+session_start();
+if (isset($view)) {
+    if ($view->errors) {
+        foreach ($view->errors as $error) {
+            echo $error;
+        }
+    }
+    if ($view->messages) {
+        foreach ($view->messages as $message) {
+            echo $message;
+        }
+    }
+}
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
   <head>
   	<?php include("views/bootstraphead.html"); ?>
   	<title>View Cases</title>
-    <link href="viewcases.css" rel="stylesheet">
+    <link href="css/viewcases.css" rel="stylesheet">
   </head>
 
   <body>
 
+    <?php include("views/logged_in_navbar.php"); ?>
     <div class="container">
-    	<?php include("views/logged_in_navbar.php"); ?>
-    	<?php include("views/view_cases.php"); ?>
 
+    	<?php include("views/view_cases.php"); ?>
 
     </div> <!-- /container -->
 
--- views/view_cases.php
+++ views/view_cases.php
@@ -1,19 +1,29 @@
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">Reports</a></li>
+            <li><a href="#">Analytics</a></li>
+            <li><a href="#">Export</a></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+            <li><a href="">Nav item</a></li>
+            <li><a href="">Nav item again</a></li>
+            <li><a href="">One more nav</a></li>
+            <li><a href="">Another nav item</a></li>
+            <li><a href="">More navigation</a></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+            <li><a href="">Nav item again</a></li>
+            <li><a href="">One more nav</a></li>
+            <li><a href="">Another nav item</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          <h1 class="page-header">Dashboard</h1>
+
 <?php
-//show potential errors / feedback (from registration object)
-ini_set('display_errors', 1);
-session_start();
-if (isset($view)) {
-    if ($view->errors) {
-        foreach ($view->errors as $error) {
-            echo $error;
-        }
-    }
-    if ($view->messages) {
-        foreach ($view->messages as $message) {
-            echo $message;
-        }
-    }
-}
 
 $db_connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 $myUsername = $_SESSION["user_name"];
@@ -52,36 +62,6 @@ if ($results === FALSE) {
 }
 
 ?>
-
-    <a href="index.php">Back to Home Page</a>
-
-<? //php echo $_SESSION['user_name']; ?>
-
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-sm-3 col-md-2 sidebar">
-          <ul class="nav nav-sidebar">
-            <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Reports</a></li>
-            <li><a href="#">Analytics</a></li>
-            <li><a href="#">Export</a></li>
-          </ul>
-          <ul class="nav nav-sidebar">
-            <li><a href="">Nav item</a></li>
-            <li><a href="">Nav item again</a></li>
-            <li><a href="">One more nav</a></li>
-            <li><a href="">Another nav item</a></li>
-            <li><a href="">More navigation</a></li>
-          </ul>
-          <ul class="nav nav-sidebar">
-            <li><a href="">Nav item again</a></li>
-            <li><a href="">One more nav</a></li>
-            <li><a href="">Another nav item</a></li>
-          </ul>
-        </div>
-        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-          <h1 class="page-header">Dashboard</h1>
-
           <div class="row placeholders">
             <div class="col-xs-6 col-sm-3 placeholder">
               <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
